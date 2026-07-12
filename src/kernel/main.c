6deb40123384c585166876e73c15342ee8b9473e
KitraMGP <3056894830@qq.com>
2026-07-12 10:39:54 +0800

    fix: 解决主函数中潜在的并发竞争问题

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,9 +1,21 @@
 #include "arch/mod.h"
 #include "lib/mod.h"
 
+volatile static bool started = false;
+
 int main()
 {
-    print_init();
-    
-    printf("cpu %d is booting!\n", mycpuid());
+    if (mycpuid() == 0)
+    {
+        print_init();
+        __sync_synchronize();
+        started = true;
+
+        printf("cpu %d is booting!\n", mycpuid());
+    } else {
+        while (!started);
+        __sync_synchronize();
+        
+        printf("cpu %d is booting!\n", mycpuid());
+    }
 }
